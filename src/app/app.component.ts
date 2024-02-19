9c6943afaeac91ed179693ce517b8799fb4499bf
EwertonLeal <ewertonolivaleal6@gmail.com>
2024-02-19 18:49:15 -0300

    criação da sessão hero do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Observable, map, startWith } from 'rxjs';
-
-export interface User {
-  name: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -12,28 +6,5 @@ export interface User {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  value = 'Clear me';
-  myControl = new FormControl<string | User>('');
-  options: User[] = [{name: 'Mary'}, {name: 'Shelley'}, {name: 'Igor'}];
-  filteredOptions!: Observable<User[]>;
-
-  ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => {
-        const name = typeof value === 'string' ? value : value?.name;
-        return name ? this._filter(name as string) : this.options.slice();
-      }),
-    );
-  }
-
-  displayFn(user: User): string {
-    return user && user.name ? user.name : '';
-  }
-
-  private _filter(name: string): User[] {
-    const filterValue = name.toLowerCase();
-
-    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
-  }
+  
 }
